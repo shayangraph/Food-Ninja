fa23019a9cd3d2458e0035fc5ce67235a1c39153
shayan jafari <shayanja12345@gmail.com>
2022-08-31 16:16:19 +0430

    add tailwind config to project and edit myself

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ff6363",
+        secondry: {
+          100: "#e2e2d5",
+          200: "#888883",
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
